a2061faafb10faff26c1b502b9217625bef2c075
Preston <preston@prestonkemp.com>
2022-01-15 11:51:41 -0600

    init

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	ed255192 "crypto/ed25519"
+	"github.com/bwmarrin/discordgo"
+	"net/http"
+)
+
+const pubKey = "dc6f78b6668e74737ab0e80d30e13beb345dd74a8aa90b4e20b14a2bb2a4341f"
+
+func main() {
+	http.HandleFunc("/interactions/", interactions)
+}
+
+func interactions(w http.ResponseWriter, r *http.Request) {
+	if verify(r) {
+
+	} else {
+		w.WriteHeader(401)
+		return
+	}
+
+	//dg, err := discordgo.New("Bot " + Token)
+	//dg.WebhookExecute()
+	//if err != nil {
+	//	fmt.Println("error creating Discord session,", err)
+	//	return
+	//}
+	//http.Redirect(w, r, "/app/login", 307)
+}
+func verify(r *http.Request) bool {
+	key := ed255192.PublicKey(pubKey)
+	return discordgo.VerifyInteraction(r, key)
+}
